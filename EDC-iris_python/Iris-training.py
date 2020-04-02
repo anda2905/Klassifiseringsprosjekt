1fb4abd7fefa819480a850915fda1fc17fe778da
Andrea <andreahoda@hotmail.com>
2020-04-02 15:00:18 +0200

    vi må vel ha en for-løkke

--- EDC-iris_python/Iris-training.py
+++ EDC-iris_python/Iris-training.py
@@ -1,11 +1,16 @@
 import numpy as np
 
-class_1 = np.loadtxt("class_1", float, delimiter=',')
-#class_2 = np.loadtxt("class_2")
-#class_3 = np.loadtxt("class_3")
+class_1 = np.loadtxt("class_1", float, delimiter=',') #Iris-setosa
+#class_2 = np.loadtxt("class_2") #Iris-versicolor
+#class_3 = np.loadtxt("class_3") #Iris-virginica
+
+N = 30
 
 class_1_train = class_1[:30,:]  #[nedover, bortover], de første 30
 class_1_test = class_1[-20:,:] #[nedover, bortover], de 20 siste
+
+for k in range(N):
+
 
 #if (class_1_test[0,:] == class_1_test[0,:]).all():
 
