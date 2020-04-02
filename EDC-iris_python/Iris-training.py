26e9998aca92c5d08859d6f35738799cb27b5507
Andrea <andreahoda@hotmail.com>
2020-04-02 15:50:49 +0200

    kommentarer/notater

--- EDC-iris_python/Iris-training.py
+++ EDC-iris_python/Iris-training.py
@@ -1,10 +1,13 @@
 import numpy as np
-
+#g_k sigmoid(x_ik)
+#x_k input data, data fra klassematrise
+#t_k targets, class labels... hmm
 class_1 = np.loadtxt("class_1", float, delimiter=',') #Iris-setosa
 #class_2 = np.loadtxt("class_2") #Iris-versicolor
 #class_3 = np.loadtxt("class_3") #Iris-virginica
 
 N = 30
+alpha = 0.1 #step factor
 
 class_1_train = class_1[:30,:]  #[nedover, bortover], de første 30
 class_1_test = class_1[-20:,:] #[nedover, bortover], de 20 siste
