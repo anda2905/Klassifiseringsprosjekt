a19949b1d05c1e484f45fd4d466a6197ba8f363a
Andrea <andreahoda@hotmail.com>
2020-04-02 12:42:26 +0200

    deleted crap

--- EDC-iris_python/Iris-training.py
+++ EDC-iris_python/Iris-training.py
@@ -1,7 +1,5 @@
-
-cl_1 = np.loadtxt("R_test2_HR71.txt", float)
-
-s_length=
 class_1 = np.loadtxt("class_1", float)
 class_2 = np.loadtxt("class_2", float)
 class_3 = np.loadtxt("class_3",float)
+
+
