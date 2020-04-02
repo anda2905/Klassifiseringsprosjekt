af3ee4c1a6e9091b9456ea09e75e3a351b4c799e
Andrea <andreahoda@hotmail.com>
2020-04-02 14:48:18 +0200

    teste at de er ulike, ikke viktig

--- EDC-iris_python/Iris-training.py
+++ EDC-iris_python/Iris-training.py
@@ -7,5 +7,7 @@ class_1 = np.loadtxt("class_1", float, delimiter=',')
 class_1_train = class_1[:30,:]  #[nedover, bortover], de første 30
 class_1_test = class_1[-20:,:] #[nedover, bortover], de 20 siste
 
+#if (class_1_test[0,:] == class_1_test[0,:]).all():
 
+print(class_1_train)
 print(class_1_test)
