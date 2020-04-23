46899d1f1b10394402511ed7d422b08033d24265
Andrea <andreahoda@hotmail.com>
2020-04-23 15:57:39 +0200

    ghj

--- EDC-vowels/Vowels.py
+++ EDC-vowels/Vowels.py
@@ -3,6 +3,6 @@ import matplotlib.pyplot as plt
 
 data = np.genfromtxt('vowdata_nohead.dat', dtype='U16')
 identifiers = data[:, 0]
-data = data[:, 7:16].astype(np.int)
+data = data[:, 1:16].astype(np.int)
 print(data)
 print(identifiers)
