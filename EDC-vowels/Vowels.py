1d64105902e84eeed6dd74c7424af7146743fb21
Andrea <andreahoda@hotmail.com>
2020-04-23 15:45:32 +0200

    skj

--- EDC-vowels/Vowels.py
+++ EDC-vowels/Vowels.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = np.loadtxt("vowdata_nohead.dat", "U16", delimiter=',')
+data = np.loadtxt("vowdata_nohead.dat", dtype = [str] + 16* [float],  delimiter=' ')
 
 print(data)
