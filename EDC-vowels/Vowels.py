97a65ad5517d2b231b5058849f0181eb0cf9367b
Tora Bjerkmo <torabjerk@hotmail.com>
2020-04-24 09:55:42 +0200

    gikk tilbake til å dele opp etter wovel

--- EDC-vowels/Vowels.py
+++ EDC-vowels/Vowels.py
@@ -14,19 +14,32 @@ N_train = 70
 N_test = 69
 N_all = N_train+N_test
 
-train_index = []
-test_index = []
 
-for vowel in vowels:
-        index = np.flatnonzero(np.core.defchararray.find(identifiers,vowel)!=-1)
-        train_index.extend(index[:N_train])
-        test_index.extend(index[N_train:])
+ae_training=data[:N_train, :]
+ae_test=data[N_train:N_all,:]
+ah_training=data[N_all:N_all+N_train, :]
+ah_test=data[N_all+N_train:2*N_all,:]
+aw_training=data[2*N_all:2*N_all + N_train, :]
+aw_test=data[2*N_all + N_train:3*N_all,:]
+eh_training=data[3*N_all:3*N_all + N_train, :]
+eh_test=data[3*N_all + N_train:4*N_all,:]
+er_training=data[4*N_all:4*N_all + N_train, :]
+er_test=data[4*N_all + N_train:5*N_all,:]
+ei_training=data[5*N_all:5*N_all + N_train, :]
+ei_test=data[5*N_all + N_train:6*N_all,:]
+ih_training=data[6*N_all:6*N_all + N_train, :]
+ih_test=data[6*N_all + N_train:7*N_all,:]
+iy_training=data[7*N_all:7*N_all + N_train, :]
+iy_test=data[7*N_all + N_train:8*N_all,:]
+oa_training=data[8*N_all:8*N_all + N_train, :]
+oa_test=data[8*N_all + N_train:9*N_all,:]
+oo_training=data[9*N_all:9*N_all + N_train, :]
+oo_test=data[9*N_all + N_train:10*N_all,:]
+uh_training=data[10*N_all:10*N_all + N_train, :]
+uh_test=data[10*N_all + N_train:11*N_all,:]
+uw_training=data[11*N_all:11*N_all + N_train, :]
+uw_test=data[11*N_all + N_train:12*N_all,:]
 
-train_set = data[train_index]
-t_k_train = data[train_index]
-test_set = data[test_index]
-t_k_test = data[test_index]
+print(uw_test)
 
 
-print(train_set)
-print(t_k_train)
